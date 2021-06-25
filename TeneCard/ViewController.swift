d8fd4528e18ad068637259d28d76c11aef31961b
Nguyen Cao Thiem <nguyen_thiem@teneocto.io>
2021-06-25 13:10:24 +0700

    Initial Commit

---
+++ TeneCard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TeneCard
+//
+//  Created by Teneocto on 25/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
