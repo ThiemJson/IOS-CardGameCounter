dc3aaf998441d8048c09f4c2161c58b08e7c6c81
Nguyen Cao Thiem <nguyen_thiem@teneocto.io>
2021-06-25 13:50:52 +0700

    App Initial

---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '13.0'
+
+target 'TeneCard' do
+
+  use_frameworks!
+
+  pod 'IQKeyboardManagerSwift'
+  pod 'SwipeCellKit'
+  pod 'RealmSwift'
+  pod 'Hero'
+end
---
+++ TeneCard/Utils/Constant.swift
@@ -0,0 +1,12 @@
+//
+//  Constant.swift
+//  TeneCard
+//
+//  Created by Teneocto on 25/06/2021.
+//
+
+import Foundation
+
+struct Constant {
+    public static var appName = "Card Score Counter"
+}
